a19f1d3100086a0be89d7f286361a7483a7a6c73
Raul Barriga <barrigaraulaug@gmail.com>
2021-01-12 22:10:21 -0800

    Added a back to top button with Material-UI from a tutorial.

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Header from './HeaderComponent'
-import AboutMe from './AboutMeComponent'
-import Services from './ServicesComponent'
-import Gallery from './GalleryComponent'
-import Form from './FormComponent'
-import Footer from './FooterComponent'
+import Header from "./HeaderComponent";
+import AboutMe from "./AboutMeComponent";
+import Services from "./ServicesComponent";
+import Gallery from "./GalleryComponent";
+import Form from "./FormComponent";
+import Footer from "./FooterComponent";
+import ScrollToTopButton from "./ScrollToTopButton";
 
 const MainComponent = () => {
-    return (
-        <>
-            <Header />
-            <main>
-                <AboutMe />
-                <Services />
-                <Gallery />
-                <Form />
-                <Footer />
-            </main>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <main>
+        <ScrollToTopButton showBelow={250} />
+        <AboutMe />
+        <Services />
+        <Gallery />
+        <Form />
+        <Footer />
+      </main>
+    </>
+  );
+};
 
-export default MainComponent
+export default MainComponent;
